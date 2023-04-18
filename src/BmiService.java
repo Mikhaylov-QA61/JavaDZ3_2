a02be0ca2cf5821bb1a334e2e22a2f5cfe7ff975
Pers test <qa61test@gmail.com>
2023-04-18 23:12:13 +0400

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public float calculate (float mass, float height) {
+    public float calculate(float mass, float height) {
         float index;
-        height=height/100;
-        index = mass/(height*height);
+        height = height / 100;
+        index = mass / (height * height);
         return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int mass = 78; // указывать занчение в килограммах
         int height = 180; // указывать значение в сантиметрах
-        int bodyMassIndex = (int) service.calculate(mass,height);
+        int bodyMassIndex = (int) service.calculate(mass, height);
         System.out.println("Ваш вес: " + mass + "кг. Ваш рост: " + height + "см.");
         System.out.println("Индекс массы тела для выших параметров = " + bodyMassIndex);
-
     }
 }
